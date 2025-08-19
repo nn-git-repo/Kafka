547c3363a6663472e4c52c77d382a48c30c2d6fd
Namratha <162692513+nn-git-repo@users.noreply.github.com>
2025-08-19 10:09:53 +0530

    Add files via upload

---
+++ OrderProcessingSystem/KafkaOrdersystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaOrdersystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaOrdersystemApplication.class, args);
+	}
+
+}
